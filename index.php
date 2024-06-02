100015751a12d6a718866a4e04a0da1856c7673f
UJ <sukach.eugene@gmail.com>
2024-06-02 17:20:15 +0300

    namespaces are added

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-require "src/router.php";
+spl_autoload_register(function (string $class_name) {
 
-$router = new Router;
+    require "src/" . str_replace("\\", "/", $class_name . ".php");
+});
+
+$router = new Framework\Router;
 
 $router->add("/home/index", ["controller" => "home", "action" => "index"]);
 $router->add("/products", ["controller" => "products", "action" => "index"]);
@@ -12,15 +15,14 @@ $router->add("/", ["controller" => "home", "action" => "index"]);
 
 $params = $router->match($path);
 
-if ($params) {
-    $action = $params["action"];
-    $controller = $params["controller"];
-
-    require "src/controllers/{$controller}.php";
-    $controller_object = new $controller; // because of the same name
-
-    $controller_object->$action();
-} else {
-
+if ($params === false) {
     exit("No route matched");
 }
+
+$action = $params["action"];
+$controller = "App\Controllers\\" . ucwords($params["controller"]);
+
+// require "src/controllers/{$controller}.php";
+$controller_object = new $controller; // because of the same file's name
+
+$controller_object->$action();
--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -1,10 +1,14 @@
 <?php
 
+namespace App\Controllers;
+
+use App\Models\Product;
+
 class Products
 {
     public function index()
     {
-        require "src/models/product.php";
+        // require "src/models/product.php";
 
         $model = new Product;
         $products = $model->getData();
