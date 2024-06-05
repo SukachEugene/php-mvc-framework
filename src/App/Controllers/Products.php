fefd4919fa4137775ed1ba69a48524113dc96ccb
UJ <sukach.eugene@gmail.com>
2024-06-05 09:57:29 +0300

    viewer templates are added

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\Product;
+use Framework\Viewer;
 
 class Products
 {
@@ -11,16 +12,21 @@ class Products
         // require "src/models/product.php";
 
         $model = new Product;
+
         $products = $model->getData();
 
-        require "views/products_index.php";
+        $viewer = new Viewer;
+
+        echo $viewer->render("shared/header.php", ["title" => "Products"]);
+        echo $viewer->render("Products/index.php", ["products" => $products]);
     }
 
     public function show(string $id)
     {
-        var_dump($id);
+        $viewer = new Viewer;
 
-        require "views/products_show.php";
+        echo $viewer->render("shared/header.php", ["title" => "Product"]);
+        echo $viewer->render("Products/show.php", ["id" => $id]);
     }
 
     public function showPage(string $title, string $id, string $page)
--- views/Products/index.php
+++ views/Products/index.php
@@ -1,11 +1,5 @@
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Products</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
+
         <h1>Products</h1>
 
         <?php foreach ($products as $product) : ?>
