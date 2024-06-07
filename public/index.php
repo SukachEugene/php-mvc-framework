3425981be9cfdd0037c4496c018e59c9bbd930eb
UJ <sukach.eugene@gmail.com>
2024-06-07 10:24:11 +0300

    added static files and public folder

--- public/index.php
+++ public/index.php
@@ -2,14 +2,15 @@
 
 declare(strict_types=1);
 
+define("ROOT_PATH", dirname(__DIR__));
 
 spl_autoload_register(function (string $class_name) {
 
-    require "src/" . str_replace("\\", "/", $class_name) . ".php";
+    require ROOT_PATH . "/src/" . str_replace("\\", "/", $class_name) . ".php";
 });
 
 $dotenv = new Framework\DotEnv;
-$dotenv->load(".env");
+$dotenv->load(ROOT_PATH . "/.env");
 
 set_error_handler("Framework\ErrorHandler::handleError");
 
@@ -22,8 +23,8 @@ if ($path === false) {
     throw new UnexpectedValueException("Malformed URL: '{$_SERVER["REQUEST_URI"]}'");
 }
 
-$router = require "config/routes.php";
-$container = require "config/services.php";
+$router = require ROOT_PATH . "/config/routes.php";
+$container = require ROOT_PATH . "/config/services.php";
 
 $dispatcher = new Framework\Dispatcher($router, $container);
 $dispatcher->handle($path);
